9459c8fa9c4827782194fe217a6a3bb78ba0881f
yuhuanzhu <yuhuanzhu@baidu.com>
2016-02-25 20:03:18 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+var cleancss = new LessPluginCleanCSS({ advanced: true });
+var autoprefix = new LessPluginAutoPrefix({ browsers: ["last 2 versions"] });
+
+var input = './src/flex-layout.less';
+var output = './dist';
+
+gulp.task('default', function () {
+    gulp.watch('./src/**.less', ['compile']);
+});
+
+gulp.task('compile', function () {
+    gulp.src(input)
+        .pipe(less({
+            plugins: [autoprefix]
+        }))
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('build', function () {
+    gulp.src(input)
+        .pipe(less({
+            plugins: [autoprefix, cleancss]
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(output));
+});
